484b759f3bed68ea7e4b1d61ec6e351112334353
QuicklyDG <quicklydeguzman@gmail.com>
2025-01-17 11:49:25 +0800

    modify stepper

---
+++ resources/js/app/pages/admin/register/sections/second-form-section.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function SecondFormSection() {
+    return (
+        <div>SecondFormSection</div>
+    )
+}
---
+++ resources/js/app/pages/admin/register/sections/third-form-section.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ThirdFormSection() {
+    return (
+        <div>ThirdFormSection</div>
+    )
+}
