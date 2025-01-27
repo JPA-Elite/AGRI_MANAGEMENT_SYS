37935fa5e2b400fb7d8a3508d247ddf558ca5bc9
Asoria-II <alejandro.sorianoii16@gmail.com>
2025-01-27 08:04:37 +0800

    Made Adjusments

--- resources/js/app/pages/admin/register/components/farm-land-component.jsx
+++ resources/js/app/pages/admin/register/components/farm-land-component.jsx
@@ -1,5 +1,6 @@
 import { setForm } from "@/app/redux/register-slice";
 import React, { useState } from "react";
+import { FaFileCirclePlus, FaTrashCan } from "react-icons/fa6";
 import { useDispatch, useSelector } from "react-redux";
 
 export default function FarmLandComponent() {
@@ -53,39 +54,24 @@ export default function FarmLandComponent() {
             </p>
 
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-12">
-                <div className="sm:col-span-2">
+                {/* <div className="sm:col-span-2">
                     <label
                         htmlFor="farmparcel"
                         className="block text-sm/6 mr-2 font-medium text-gray-900"
                     >
                         No. of Farm Parcel:
                     </label>
-                    {/* <input
+                    <input
                         id="farmparcel"
                         name="farmparcel"
                         type="number"
                         autoComplete="given-name"
                         className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-none placeholder:text-gray-400 focus:ring-green-500 focus:border-green-500 sm:text-sm/6"
-                    /> */}
-                    <button
-                        onClick={() =>
-                            dispatch(
-                                setForm({
-                                    ...form,
-                                    parcels: [
-                                        ...form.parcels, // Spread the current parcels
-                                        {}, // Add a new empty parcel object
-                                    ],
-                                })
-                            )
-                        }
-                        className="bg-green-600 p-2 rounded-md text-white"
-                    >
-                        No. of Farm Parcel:
-                    </button>
-                </div>
+                    />
+                    
+                </div> */}
 
-                <div className="sm:col-span-10">
+                <div className="sm:col-span-12">
                     <label
                         htmlFor="farmparcel"
                         className="block text-sm/6 mr-2 font-medium text-gray-900"
@@ -116,6 +102,27 @@ export default function FarmLandComponent() {
                         />
                     </div>
                 </div>
+                <div className="sm:col-span-12">
+                <button
+                        onClick={() =>
+                            dispatch(
+                                setForm({
+                                    ...form,
+                                    parcels: [
+                                        ...form.parcels, // Spread the current parcels
+                                        {}, // Add a new empty parcel object
+                                    ],
+                                })
+                            )
+                        }
+                        className="rounded-full bg-white px-3.5 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-500 hover:bg-green-500 hover:text-white"
+                    >
+                        <div className="flex flex-row text-center">
+                        <FaFileCirclePlus className="mr-2 size-6"/>
+                        Add Farm Parcel Form
+                        </div>
+                    </button>
+                </div>
 
                 {form.parcels.map((res, i) => {
                     const number = i + 1;
@@ -128,9 +135,10 @@ export default function FarmLandComponent() {
                                 {i !== 0 && (
                                     <button
                                         onClick={() => deleteParcel(i)}
-                                        className="bg-red-600 text-white p-2 rounded-md mt-2"
+                                        title="Remove Parcel Form"
+                                        className="rounded-full bg-red-600 p-2 text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
                                     >
-                                        Delete Parcel
+                                        <FaTrashCan className="size-5"/>
                                     </button>
                                 )}
                             </div>
--- resources/js/app/pages/admin/register/sections/third-form-section.jsx
+++ resources/js/app/pages/admin/register/sections/third-form-section.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import OwnershipDocumentComponent from '../components/ownership-document-component'
 import FarmLandComponent from '../components/farm-land-component'
 
 export default function ThirdFormSection() {
     return (
         <div>
             <FarmLandComponent/>
-            <OwnershipDocumentComponent/>
         </div>
     )
 }
