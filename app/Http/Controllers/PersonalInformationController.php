1356ebafc47fadc98a5a115953b52323eb0ed670
Marlou Dev <marlou.dev.app@gmail.com>
2025-02-14 14:20:16 +0800

    added nullable

--- app/Http/Controllers/PersonalInformationController.php
+++ app/Http/Controllers/PersonalInformationController.php
@@ -24,64 +24,64 @@ class PersonalInformationController extends Controller
     public function store(Request $request)
     {
         $personalInfo = PersonalInformation::create([
-            'firstname' => $request->personal_info['firstname'],
-            'civil' => $request->personal_info['civil'],
-            'contact_person' => $request->personal_info['contact_person'],
-            'contact_person_mobile' => $request->personal_info['contact_person_mobile'],
-            'dob' => $request->personal_info['dob'],
-            'gender' => $request->personal_info['gender'],
-            'landline' => $request->personal_info['landline'],
-            'lastname' => $request->personal_info['lastname'],
-            'mobile' => $request->personal_info['mobile'],
-            'middlename' => $request->personal_info['middlename'],
-            'mothers_maiden' => $request->personal_info['mothers_maiden'],
-            'pob' => $request->personal_info['pob'],
-            'religion' => $request->personal_info['religion'],
-            'suffix' => $request->personal_info['suffix'],
+            'firstname' => $request->personal_info['firstname'] ?? null,
+            'civil' => $request->personal_info['civil'] ?? null,
+            'contact_person' => $request->personal_info['contact_person'] ?? null,
+            'contact_person_mobile' => $request->personal_info['contact_person_mobile'] ?? null,
+            'dob' => $request->personal_info['dob'] ?? null,
+            'gender' => $request->personal_info['gender'] ?? null,
+            'landline' => $request->personal_info['landline'] ?? null,
+            'lastname' => $request->personal_info['lastname'] ?? null,
+            'mobile' => $request->personal_info['mobile'] ?? null,
+            'middlename' => $request->personal_info['middlename'] ?? null,
+            'mothers_maiden' => $request->personal_info['mothers_maiden'] ?? null,
+            'pob' => $request->personal_info['pob'] ?? null,
+            'religion' => $request->personal_info['religion'] ?? null,
+            'suffix' => $request->personal_info['suffix'] ?? null,
         ]);
 
         Household::create([
             'register_id' => $personalInfo->id,
-            'household_head' => $request->personal_info['household_head'],
-            'household_head_name' => $request->personal_info['household_head_name']??null,
-            'household_relation' => $request->personal_info['household_relation']??null,
-            'male_members' => $request->personal_info['male_members'],
-            'female_members' => $request->personal_info['female_members'],
-            'annual_income' => $request->personal_info['annual_income']??null,
+            'household_head' => $request->personal_info['household_head'] ?? null,
+            'household_head_name' => $request->personal_info['household_head_name'] ?? null,
+            'household_relation' => $request->personal_info['household_relation'] ?? null,
+            'male_members' => $request->personal_info['male_members'] ?? null,
+            'female_members' => $request->personal_info['female_members'] ?? null,
+            'annual_income' => $request->personal_info['annual_income'] ?? null,
         ]);
 
         GovernmentAffiliation::create([
             'register_id' => $personalInfo->id,
-            'PWD' => $request->government_affiliation['PWD'],
-            '4Ps' => $request->government_affiliation['4Ps'],
-            'indigenous_group' => $request->government_affiliation['indigenous_group'],
-            'indigenous_group_name' => $request->government_affiliation['indigenous_group_name'],
-            'government_id' => $request->government_affiliation['government_id'],
-            'government_id_type' => $request->government_affiliation['government_id_type'],
-            'government_id_number' => $request->government_affiliation['government_id_number'],
-            'farmers_association' => $request->government_affiliation['farmers_association'],
+            'PWD' => $request->government_affiliation['PWD'] ?? null,
+            '4Ps' => $request->government_affiliation['4Ps'] ?? null,
+            'indigenous_group' => $request->government_affiliation['indigenous_group'] ?? null,
+            'indigenous_group_name' => $request->government_affiliation['indigenous_group_name'] ?? null,
+            'government_id' => $request->government_affiliation['government_id'] ?? null,
+            'government_id_type' => $request->government_affiliation['government_id_type'] ?? null,
+            'government_id_number' => $request->government_affiliation['government_id_number'] ?? null,
+            'farmers_association' => $request->government_affiliation['farmers_association'] ?? null,
             'farmers_association_name' => $request->government_affiliation['farmers_association_name'] ?? null,
         ]);
 
         FarmProfile::create([
             'register_id' => $personalInfo->id,
-            'main_livelihood' => $request->main_livelihood,
-            'farm_activity' => $request->farm_activity,
+            'main_livelihood' => $request->personal_info['main_livelihood'] ?? null,
+            'farm_activity' => $request->personal_info['farm_activity'] ?? null,
         ]);
         foreach ($request->land_farmers as $key => $value) {
             LandFarmer::create([
                 'register_id' => $personalInfo->id,
-                'farmer_rotation_name' => $value['name'],
+                'farmer_rotation_name' => $value['name'] ?? null,
             ]);
         }
 
         foreach ($request->parcels as $key => $value) {
             $parcel = Parcel::create([
                 'register_id' => $personalInfo->id,
-                'farm_location' => $value['farmlocation'],
-                'farm_area' => $value['farmarea'],
-                'ancestral_domain' => $value['ancestraldomain'],
-                'agrarian_beneficiary' => $value['agrarianreform'],
+                'farm_location' => $value['farmlocation'] ?? null,
+                'farm_area' => $value['farmarea'] ?? null,
+                'ancestral_domain' => $value['ancestraldomain'] ?? null,
+                'agrarian_beneficiary' => $value['agrarianreform'] ?? null,
                 'ownership_doc_number' => $value['ownership_doc_number'] ?? '',
                 'ownership_type' => $value['ownership_type'] ?? null,
             ]);
@@ -89,12 +89,12 @@ class PersonalInformationController extends Controller
                 ParcelComponent::create([
                     'register_id' => $personalInfo->id,
                     'parcel_id' => $parcel->id,
-                    'commodity' => $val['cropcommodity'],
-                    'size' => $val['sizeha'],
-                    'number_heads' => $val['heads'],
-                    'farm_type' => $val['farmtype'],
-                    'organic_practioner' => $val['organicpractitioner'],
-                    'remarks' => $val['remarks'],
+                    'commodity' => $val['cropcommodity'] ?? null,
+                    'size' => $val['sizeha'] ?? null,
+                    'number_heads' => $val['heads'] ?? null,
+                    'farm_type' => $val['farmtype'] ?? null,
+                    'organic_practioner' => $val['organicpractitioner'] ?? null,
+                    'remarks' => $val['remarks'] ?? null,
                 ]);
             }
         }
