b3dd2fcf09145e4fc29c43864efb57b4812d3349
Marlou Dev <marlou.dev.app@gmail.com>
2025-03-20 19:57:49 +0800

    added new build

---
+++ public/build/assets/active-beneficiary-section-BFDcJRgl.js
@@ -0,0 +1,1 @@
+import{j as e}from"./app-Cod8W422.js";import i from"./beneficiary-table-component-DPJaIk5m.js";function n(){return e.jsx("div",{children:e.jsx(i,{})})}export{n as default};
---
+++ public/build/assets/preview-form-section-BhDFgbA_.js
@@ -0,0 +1,1 @@
+import{j as e}from"./app-Cod8W422.js";function i(){return e.jsx("div",{children:"PreviewFormSection"})}export{i as default};
