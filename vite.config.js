c18af9da49eb5221a4e9884c8b06218d4ed7eb82
joshxb2 <algadipej962@gmail.com>
2025-04-17 19:06:14 +0800

    Add built Vite assets

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     build: {
-        chunkSizeWarningLimit: 99999999999, // Adjust chunk size warning limit if needed
-        manifest: true,
-        outDir: 'public/build',
-        rollupOptions: {
-            input: 'resources/js/app.jsx',
-        },
+        chunkSizeWarningLimit: 99999999999,
+        // manifest: true,
+        // outDir: 'public/build',
+        // rollupOptions: {
+        //     input: 'resources/js/app.jsx',
+        // },
     },
     plugins: [
         laravel({
