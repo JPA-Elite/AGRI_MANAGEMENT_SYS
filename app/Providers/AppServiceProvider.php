b8039536c840cc165678830b71df0666bee67df3
joshxb2 <algadipej962@gmail.com>
2025-04-18 22:18:17 +0800

    update provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Log::info(app()->environment());
+        Log::info("Logs: " . app()->environment());
         Schema::defaultStringLength(191);
 
         if (app()->environment('production')) {
